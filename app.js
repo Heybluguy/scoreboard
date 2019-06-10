091372884ed2d1836c03d6a9587f491511a6113b
Luis  Senior <heybluguy@yahoo.com>
2019-06-09 23:21:19 -0500

    using components

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-
-const desc = 'I just learned how to create a react node and render it into the DOM.';
-
-const myTitleID = 'main-title';
-
-const name = 'Luis';
-
-const header = (
-  <header>
-    <h1 id='mainTitleID'>{ name }'s first React element.</h1>
-    <p>{ desc }</p>
-  </header>
-);
-
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
 ReactDOM.render(
-  header,
+  <Header />,
   document.getElementById('root')
 );
